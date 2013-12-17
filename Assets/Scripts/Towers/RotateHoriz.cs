1390c957d080371f021a13aa15b0a6f6363201b3
tino113 <tino113@gmail.com>
2013-12-17 23:43:07 +0000

    Added Unity Project Files.

---
+++ Assets/Scripts/Towers/RotateHoriz.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateHoriz : MonoBehaviour {
+
+	public GameObject target;
+	[Range(1, 10)] public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per 
+	void Update()
+	{
+		Vector3 tmpPos = new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z);
+		transform.LookAt(tmpPos);
+
+	}
+}
