566d112c565055a85d765ff5173ebe649d90cc6c
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-12-19 23:47:21 +0000

    Added very basic camera

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public float speed;
+
+	void Start () {
+
+	}
+	
+	void FixedUpdate () {
+		float move_h = Input.GetAxis("Horizontal");
+		float move_v = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (move_h, 0.0f, move_v);
+		transform.position += movement * speed * Time.deltaTime;
+
+
+	}
+}
