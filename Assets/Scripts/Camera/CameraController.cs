d4534844c5512826215db9e8e92a4d8dae4f2dbc
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-12-20 14:06:18 +0000

    Refactor camera controller.
    Some method names/signatures could be better.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -25,27 +25,36 @@ public class CameraController : MonoBehaviour {
 	}
 
 	void Update () {
+		UpdateFromInput ();
+		
+		UpdateCamera (
+			MoveLerp (transform.position.x, horizontalMove),
+		    ZoomLerp (transform.position.y, minHeight, maxHeight),
+		    MoveLerp (transform.position.z, verticalMove),
+		    ZoomLerp (transform.localRotation.eulerAngles.x, minRotate, maxRotate),
+		    ZoomLerp (camera.fieldOfView, minFoV, maxFoV)
+		);
+	}
+	
+	private float MoveLerp(float from, float to) {
+		return Mathf.Lerp (from, to, Time.deltaTime * moveDamping);
+	}
+	
+	private float ZoomLerp(float from, float min, float max) {
+		float to = (((max - min) / 100) * zoom) + min;
+		return Mathf.Lerp (from, to, Time.deltaTime * zoomDamping);
+	}
 
+	private void UpdateCamera(float x, float y, float z, float rotate, float fieldOfView) {
+		transform.localRotation = Quaternion.Euler(rotate, 0, 0);
+		transform.position = new Vector3 (x, y, z);
+		camera.fieldOfView = fieldOfView;
+	}
+
+	private void UpdateFromInput(){
 		horizontalMove += Input.GetAxis("Horizontal") * moveSpeed;
 		verticalMove += Input.GetAxis("Vertical") * moveSpeed;
-
-		float _x = Mathf.Lerp (transform.position.x, horizontalMove, Time.deltaTime * moveDamping);
-		float _z = Mathf.Lerp (transform.position.z, verticalMove, Time.deltaTime * moveDamping);
-
-
 		zoom += Input.GetAxis ("Mouse ScrollWheel") * 40f;
 		zoom = Mathf.Clamp (zoom, 0, 100);
-
-		float targetHeight = (((maxHeight - minHeight) / 100) * zoom) + minHeight;
-		float _y = Mathf.Lerp (transform.position.y, targetHeight, Time.deltaTime * zoomDamping);
-		float FoVTarget = (((maxFoV - minFoV) / 100) * zoom) + minFoV;
-		float _fov = Mathf.Lerp (camera.fieldOfView, FoVTarget, Time.deltaTime * zoomDamping);
-
-		float rotationTarget = (((maxRotate - minRotate) / 100) * zoom) + minRotate;
-		float _rotate = Mathf.Lerp (transform.localRotation.eulerAngles.x, rotationTarget, Time.deltaTime * zoomDamping);
-		transform.localRotation = Quaternion.Euler(_rotate, 0, 0);
-
-		transform.position = new Vector3 (_x, _y, _z);
-		camera.fieldOfView = _fov;
 	}
 }
