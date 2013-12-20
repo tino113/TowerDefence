b72eee3e5977807f84258cbecf63e24a7adf04dc
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-12-20 01:25:40 +0000

    Added swing to camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 	
 	public float speed;
-
+	
 	void Start () {
-
+		
 	}
 	
 	void FixedUpdate () {
@@ -16,13 +16,15 @@ public class CameraController : MonoBehaviour {
 		
 		Vector3 movement = new Vector3 (horizontalMove, 0.0f, verticalMove);
 		transform.position += movement * speed * Time.deltaTime;
-
-		if (mouseScroll < 0) {
+		
+		if (mouseScroll < 0 && camera.fieldOfView < 100) {
 			camera.fieldOfView = Mathf.Min(camera.fieldOfView+2, 100);
+			transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * Quaternion.Euler(2, 0, 0), 5f);
 		}
-
-		if (mouseScroll > 0) {
+		
+		if (mouseScroll > 0 && camera.fieldOfView > 40) {
 			camera.fieldOfView = Mathf.Max(camera.fieldOfView-2, 40);
+			transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * Quaternion.Euler(-2, 0, 0), 5f);
 		}
 	}
 }
