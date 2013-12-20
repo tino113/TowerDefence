ac2280ce928a75411d3ebf5978b6c16648cde68f
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-12-20 13:44:11 +0000

    More work on camera, rotation added

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,47 +4,48 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	private float zoomSpeed = 5f;
-	private float minZoom = 40;
-	private float maxZoom = 100;
+	private float zoomDamping = 6f;
+	
+	private float zoom = 40f;
 
-	//private float sensitivityDistance = 7.5f;
+	private float minHeight = 5f;
+	private float maxHeight = 25f;
+	private float minFoV = 40f;
+	private float maxFoV = 60f;
+	private float minRotate = 7f;
+	private float maxRotate = 80f;
 
-	private float moveSpeed = 5f;
-	private float damping = 0.02f;
-	private float cameraTargetField;
-
+	private float moveSpeed = 1f;
 	private float horizontalMove = 0f;
 	private float verticalMove = 0f;
+	private float moveDamping = 20f;
 
-	void Start () {
-		cameraTargetField = camera.fieldOfView;
+	void Start(){
+		transform.localRotation = Quaternion.Euler ((((maxRotate - minRotate) / 100) * zoom) + minRotate, 0, 0);
 	}
 
 	void Update () {
+
 		horizontalMove += Input.GetAxis("Horizontal") * moveSpeed;
 		verticalMove += Input.GetAxis("Vertical") * moveSpeed;
-		float mouseScroll = (Input.GetAxis ("Mouse ScrollWheel")*10) * zoomSpeed;
 
-		float _x = Mathf.Lerp (transform.position.x, horizontalMove, Time.deltaTime * damping);
-		float _z = Mathf.Lerp (transform.position.z, verticalMove, Time.deltaTime * damping);
+		float _x = Mathf.Lerp (transform.position.x, horizontalMove, Time.deltaTime * moveDamping);
+		float _z = Mathf.Lerp (transform.position.z, verticalMove, Time.deltaTime * moveDamping);
 
-		// zoom in
-		if (mouseScroll > 0 && cameraTargetField >= minZoom) {
-			cameraTargetField += mouseScroll;
-			//if (cameraTargetField > minZoom) {
-			//	transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * Quaternion.Euler(2, 0, 0), 5f);
-			//}
 
-		// zoom out
-		} else if (mouseScroll < 0 && cameraTargetField <= maxZoom) {
-			cameraTargetField += mouseScroll;
-			//if (cameraTargetField < maxZoom) {
-			//	transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * Quaternion.Euler(-2, 0, 0), 5f);
-			//}
-		}
+		zoom += Input.GetAxis ("Mouse ScrollWheel") * 40f;
+		zoom = Mathf.Clamp (zoom, 0, 100);
 
-		transform.position = new Vector3 (_x, 7, _z);
-		cameraTargetField = Mathf.Clamp (cameraTargetField, minZoom, maxZoom);
-		camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, cameraTargetField, Time.deltaTime); 
+		float targetHeight = (((maxHeight - minHeight) / 100) * zoom) + minHeight;
+		float _y = Mathf.Lerp (transform.position.y, targetHeight, Time.deltaTime * zoomDamping);
+		float FoVTarget = (((maxFoV - minFoV) / 100) * zoom) + minFoV;
+		float _fov = Mathf.Lerp (camera.fieldOfView, FoVTarget, Time.deltaTime * zoomDamping);
+
+		float rotationTarget = (((maxRotate - minRotate) / 100) * zoom) + minRotate;
+		float _rotate = Mathf.Lerp (transform.localRotation.eulerAngles.x, rotationTarget, Time.deltaTime * zoomDamping);
+		transform.localRotation = Quaternion.Euler(_rotate, 0, 0);
+
+		transform.position = new Vector3 (_x, _y, _z);
+		camera.fieldOfView = _fov;
 	}
 }
